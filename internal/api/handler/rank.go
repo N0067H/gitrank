d10030f61af389714fee695a1af2c90a028b4eeb
SeungYeop Ryu <n006h3417h@gmail.com>
2025-06-30 17:45:51 +0900

    fix: fix returning json as raw string

--- internal/api/handler/rank.go
+++ internal/api/handler/rank.go
@@ -1,12 +1,18 @@
 package handler
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	myredis "github.com/n0067h/gitrank/internal/redis"
 	"github.com/redis/go-redis/v9"
 )
+
+type Contributer struct {
+	Login              string `json:"Login"`
+	TotalContributions int    `json:"TotalContributions"`
+}
 
 func GetRank(c *fiber.Ctx) error {
 	// cache check
@@ -22,5 +28,10 @@ func GetRank(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusInternalServerError)
 	}
 
-	return c.SendString(ranking)
+	var result []Contributer
+	if err := json.Unmarshal([]byte(ranking), &result); err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(result)
 }
